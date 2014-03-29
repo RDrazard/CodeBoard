0249ab4c6c51e97c526fda95ddb813995757abf0
Alan Plotko <alan.plotko@gmail.com>
2014-03-29 10:02:31 -0400

    Fix tab issues

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,8 @@ import os
 
 def xfile(afile, global_vars=None, local_vars=None):
 	with open(afile) as f:
-    	code = compile(f.read(), "somefile.py", 'exec')
-    	exec(code, global_vars, local_vars)
+		code = compile(f.read(), "somefile.py", 'exec')
+		exec(code, global_vars, local_vars)
 
 virtenv = os.environ['APPDIR'] + '/virtenv/'
 os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
