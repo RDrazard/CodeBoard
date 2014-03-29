b0198d4522b0df8fce34c40b13aae4f4f60cc2e9
Alan Plotko <alan.plotko@gmail.com>
2014-03-29 19:26:13 -0400

    Fix bug maybe?

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,17 @@
 
 import os
 
+def xfile(afile, global_vars=None, local_vars=None):
+	with open(afile) as f:
+		code = compile(f.read(), "somefile.py", 'exec')
+		exec(code, global_vars, local_vars)
+
 virtenv = os.environ['APPDIR'] + '/virtenv/'
 os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-    execfile(virtualenv, dict(__file__=virtualenv))
+    xfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
 
-from codeboardapp import application
+import codeboard.application
