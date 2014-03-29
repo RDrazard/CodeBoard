415babede64a28442d44fbf2e62735ceb41367a8
Alan Plotko <alan.plotko@gmail.com>
2014-03-29 19:24:18 -0400

    Fix bug maybe?

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,11 @@
 
 import os
 
-def xfile(afile, global_vars=None, local_vars=None):
-	with open(afile) as f:
-		code = compile(f.read(), "somefile.py", 'exec')
-		exec(code, global_vars, local_vars)
-
 virtenv = os.environ['APPDIR'] + '/virtenv/'
 os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-    xfile(virtualenv, dict(__file__=virtualenv))
+    execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
 
