5f4eadf79d77bdcc9f6f8c48a6510e4429b7c410
Alan Plotko <alan.plotko@gmail.com>
2014-03-29 10:15:08 -0400

    Serve static files differently

--- twtapp.py
+++ twtapp.py
@@ -257,10 +257,21 @@ def dbg_env():
               for key, value in sorted(os.environ.items())]
   return "<pre>env is\n%s</pre>" % '\n'.join(env_list)
 
-@bottle.route('/static/:filename')
-def static_file(filename):
-  bottle.send_file(filename,
-                   root= os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
-                                      'static'))
+""" Static routes for serving files in the assets directory """
+@bottle.get('/<filename:re:.*\.js>')
+def scripts(filename):
+    return static_file(filename, root=os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/js'))
+@bottle.get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root=os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/css'))
+@bottle.get('/<filename:re:.*\.(jpg|png|gif)>')
+def images(filename):
+    return static_file(filename, root=os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/img'))
+@bottle.get('/<filename:re:.*\.(ico)>')
+def favicons(filename):
+    return static_file(filename, root=os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/ico'))
+@bottle.get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root=os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/fonts'))
 
 application = bottle.default_app()
