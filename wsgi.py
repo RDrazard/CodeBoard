c48ba6a787c7094e28e0d694fc985c5b930d3775
Alan Plotko <alan.plotko@gmail.com>
2014-03-29 09:49:31 -0400

    Fix for Python 3.3

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,15 @@
 
 import os
 
+def xfile(afile, globalz=None, localz=None):
+    with open(afile, "r") as fh:
+        exec(fh.read(), globalz, localz)
+
 virtenv = os.environ['APPDIR'] + '/virtenv/'
 os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-    execfile(virtualenv, dict(__file__=virtualenv))
+    xfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
     pass
 
