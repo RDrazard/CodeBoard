01ee054109afdbefee2bb34aba67976fc9d484d0
Alan Plotko <alan.plotko@gmail.com>
2014-03-29 22:28:01 -0400

    Update imports

--- codeboardapp.py
+++ codeboardapp.py
@@ -260,63 +260,6 @@ def post_signup():
 			   error_signup=True,
 			   logged=False)
 
-# auth managers setup, pretty straightforward
-
-twitter = Twitter(
-            settings.TWITTER_KEY,
-            settings.TWITTER_SECRET,
-            settings.TWITTER_CALLBACK)
-
-facebook = Facebook(
-            settings.FACEBOOK_KEY,
-            settings.FACEBOOK_SECRET,
-            settings.FACEBOOK_CALLBACK,
-            settings.FACEBOOK_SCOPE)
-
-google = Google(
-            settings.GOOGLE_KEY,
-            settings.GOOGLE_SECRET,
-            settings.GOOGLE_CALLBACK,
-            settings.GOOGLE_SCOPE)
-
-# this mapping will be used later
-mapping = {'twitter': twitter, 'facebook': facebook, 'google': google}
-
-
-# simple wrapper to let Bottle talk to the tastyauth managers
-class CookieMonster(object):
-    def get_cookie(self, name, default=None):
-        return request.get_cookie(name, default)
-
-    def set_cookie(self, name, value):
-        response.set_cookie(name, value)
-
-    def delete_cookie(self, name):
-        response.delete_cookie(name)
-
-@route('/facebook/login')
-def facebook_login():
-    url = facebook.redirect(request.environ)
-    redirect(url)
-
-@route('/google/login')
-def google_login():
-    url = google.redirect(request.environ)
-    redirect(url)
-
-@route('/:provider/callback')
-def provider_callback(provider):
-    try:
-        user = mapping[provider].get_user(request.environ)
-    except UserDenied:
-        # the user refused to log
-        return 'User denied'
-    except NegotiationError:
-        # oops, something nasty happend
-        return 'Negotiation error, maybe expired stuff'
-
-    return '<pre>{0}</pre>'.format(pformat(user))
-
 @bottle.route('/DEBUG/cwd')
 def dbg_cwd():
   return "<tt>cwd is %s</tt>" % os.getcwd()
